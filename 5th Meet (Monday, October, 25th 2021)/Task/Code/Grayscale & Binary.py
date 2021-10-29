f723f6e4c09294d77dbf24b412ae086dae3f8e89
LuceRest <lucerestu702@gmail.com>
2021-10-30 00:03:49 +0700

    Edit rgb2Binary function and it's work

--- 5th Meet (Monday, October, 25th 2021)/Task/Code/Grayscale & Binary.py
+++ 5th Meet (Monday, October, 25th 2021)/Task/Code/Grayscale & Binary.py
@@ -33,36 +33,6 @@ def browseImage():
 def rgb2Gray():
     global fln
 
-    print("\n\n{}\n\n".format(fln))
-    
-    img = mpimg.imread(fln)
-    print("\n\n{}\n\n".format(img))
-    plt.imshow(img)
-
-
-    grayImage = np.zeros(img.shape)
-    R = np.array(img[:, :, 0])
-    G = np.array(img[:, :, 1])
-    B = np.array(img[:, :, 2])
-
-    R = (R * .299)
-    G = (G * .587)
-    B = (B * .114)
-
-    Avg = (R+G+B)
-    grayImage = img.copy()
-
-    for i in range(3):
-        grayImage[:,:,i] = Avg
-
-    imgTk = ImageTk.PhotoImage(grayImage)
-    lbl.configure(image=imgTk)
-    lbl.image = imgTk
-        
-
-def rgb2Gray2():
-    global fln
-
     img = Image.open(fln)
 
     for x in range(img.size[0]):
@@ -78,80 +48,11 @@ def rgb2Gray2():
     lbl.configure(image=imgTk)
     lbl.image = imgTk
 
-        
+
 def rgb2Binary():
     global fln
     
-    img = cv2.imread(fln)
-    height, width, channels = img.shape
-
-    imgBinary = np.zeros((height,width,1))
-    imgGrayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    thresh = 120
-    for i in np.arange(height):
-        for j in np.arange(width):
-            x = imgGrayscale.item(i,j)
-            if x >= thresh:
-                y = 1
-            else :
-                y = 0
-
-            imgBinary.itemset((i,j,0),int(y))
-
-    imgPill = Image.fromarray(imgBinary)
-    imgTk = ImageTk.PhotoImage(imgPill)
-    lbl.configure(image=imgTk)
-    lbl.image = imgTk
-
-
-def rgb2Binary2():
-    global fln
-    
-    img = Image.open(fln)
-    
-    for x in range(img.size[0]):
-        for y in range(img.size[1]):
-            r,g,b = img.getpixel((x,y))
-            r = (r * .299)
-            g = (g * .587)
-            b = (b * .114)
-            sum = int((r+g+b))
-            img.putpixel((x,y), (sum, sum, sum))
-
-    imgBinary = np.zeros((img.size[0],img.size[1],1))
-
-    thresh = 120
-    for i in np.arange(img.size[0]):
-        for j in np.arange(img.size[1]):
-            x = img.getpixel((i,j))[0]
-            if x >= thresh:
-                y = 1
-            else :
-                y = 0
-
-            imgBinary.itemset((i,j,0),int(y))
-            # img.putpixel((x,y), (y, y, y))
-
-    print(f'\n{imgBinary}\n-------------')
-
-    imgNp = np.asarray(imgBinary)
-    print(f'\n{imgNp}\n-------------')
-
-    imgPill = Image.fromarray(imgNp, mode="L")
-    print(f'\n{imgPill}\n-------------')
-
-    imgTk = ImageTk.BitmapImage(imgPill)
-    print(f'\n{imgTk}\n-------------')
-
-    lbl.configure(image=imgTk)
-    lbl.image = imgTk
-
-
-def rgb2Binary3():
-    global fln
-    
-    imgGrayscale = Image.open('mountain.jpg').convert('L')
+    imgGrayscale = Image.open(fln).convert('L')
     pxGrayscale = imgGrayscale.load()
 
     horizontal = imgGrayscale.size[0]
@@ -197,10 +98,10 @@ if __name__ == '__main__':
     btn = Button(frm, text="Browser Image", command=browseImage)
     btn.pack(side=tk.LEFT)
 
-    btnGray = Button(frm, text="Convert to Grayscale", command=rgb2Gray2)
+    btnGray = Button(frm, text="Convert to Grayscale", command=rgb2Gray)
     btnGray.pack(side=tk.LEFT)
 
-    btnBinary = Button(frm, text="Convert to Binary", command=rgb2Binary3)
+    btnBinary = Button(frm, text="Convert to Binary", command=rgb2Binary)
     btnBinary.pack(side=tk.LEFT)
     
     btnExit = Button(frm, text="Exit", command=lambda: exit())
