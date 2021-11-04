bc597801943e410182200c20d275c3759f20556f
LuceRest <lucerestu702@gmail.com>
2021-11-05 00:27:37 +0700

    Edit UI, add darkening function, and add exit function

--- 6th Meet (Monday, November, 1st 2021)/Task/Code/Brightening.py
+++ 6th Meet (Monday, November, 1st 2021)/Task/Code/Brightening.py
@@ -77,6 +77,28 @@ def brightening():
             px[x, y] = (r, g, b)
     
     setResult(img)
+    valBright.delete(0, END)
+
+
+def darkening():
+    global fln
+    
+    img = Image.open(fln)
+    px = img.load()
+
+    horizontal = img.size[0]
+    vertical = img.size[1]
+
+    for x in range(horizontal):
+        for y in range(vertical):
+            r = clipping(px[x, y][0] - int(valBright.get()))
+            g = clipping(px[x, y][1] - int(valBright.get()))
+            b = clipping(px[x, y][2] - int(valBright.get()))
+            px[x, y] = (r, g, b)
+    
+    setResult(img)
+    valBright.delete(0, END)
+
 
 
 def brighteningSlider(e):
@@ -130,20 +152,23 @@ if __name__ == '__main__':
     btnBrowse = ttk.Button(frmBtn, text='Browse Image', style='info.TButton', cursor="hand2", command=browseImage)
     btnBrowse.grid(row=0, column=0, columnspan=2, padx=15)
 
-    btnExit = ttk.Button(frmBtn, text='Exit', style='danger.TButton', cursor="hand2")
+    btnExit = ttk.Button(frmBtn, text='Exit', style='danger.TButton', cursor="hand2", command=lambda: exit())
     btnExit.grid(row=0, column=2, columnspan=2, padx=20)
 
-    valBright = ttk.Entry(frmBtn, style='info.TEntry')
-    valBright.grid(row=1, column=0, columnspan=2, padx=15, pady=10)
+    btnDark = ttk.Button(frmBtn, text='-', style='info.TButton', cursor="hand2", command=darkening)
+    btnDark.grid(row=1, column=0, columnspan=2, padx=20, pady=10)
 
-    btnBright = ttk.Button(frmBtn, text='Brightening', style='info.TButton', cursor="hand2", command=brightening)
+    valBright = ttk.Entry(frmBtn, style='info.TEntry')
+    valBright.grid(row=1, column=1, columnspan=2, padx=15, pady=10)
+
+    btnBright = ttk.Button(frmBtn, text='+', style='info.TButton', cursor="hand2", command=brightening)
     btnBright.grid(row=1, column=2, columnspan=2, padx=20, pady=10)
 
     sldBright = ttk.Scale(frmBtn, from_=-255, to=255, value=0, orient='horizontal', style='info.Horizontal.TScale', length=511, command=brighteningSlider)
     sldBright.grid(row=2, column=0, columnspan=4, padx=20, pady=10)
 
 
-    window.title("Image Browser App - 5200411488")
+    window.title("Image Brightness Setting - 5200411488")
     window.geometry("1280x720")
     # window.resizable(0, 0)
     window.mainloop()
