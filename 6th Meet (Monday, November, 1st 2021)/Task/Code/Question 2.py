6557ae6b5cf21c78053915850be78e038230a11d
LuceRest <lucerestu702@gmail.com>
2021-11-05 01:31:07 +0700

    Edit Question 2.py

--- 6th Meet (Monday, November, 1st 2021)/Task/Code/Question 2.py
+++ 6th Meet (Monday, November, 1st 2021)/Task/Code/Question 2.py
@@ -1,18 +1,25 @@
 import cv2
 
 # Reading image file
-img = cv2.imread('6th Meet (Monday, November, 1st 2021)\Task\Resource\mountain.jpg')
+img = cv2.imread('lena.png')
 cv2.imshow('ori.jpg', img)
 cv2.waitKey(0)
 
+print(f'\nImg : {img}\n---------------')
+
 # Applying NumPy scalar multiplication on image
-fimg = cv2.divide(img, 1.5)
+
+imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+print(f'\nImgRGB : {imgRGB}\n---------------')
+
+fimg = cv2.divide(imgRGB, 0.9)
+cv2.imshow('result.jpg', fimg)
+print(f'\nfimg : {fimg}\n---------------')
 
 
 # Saving the output image
-cv2.imwrite('darkerLib.jpg', fimg)
-img_result = cv2.imread('darkerLib.jpg')
-cv2.imshow('result.jpg', img_result)
+# cv2.imwrite('darkerLib.jpg', fimg)
+# img_result = cv2.imread('darkerLib.jpg')
 
 
 cv2.waitKey(0)
