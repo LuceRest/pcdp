ffa278bfd98f6dcda28218e098cb173efb25abb0
LuceRest <lucerestu702@gmail.com>
2021-11-08 15:59:12 +0700

    Initial 7th meet

---
+++ 7th Meet (Monday, November, 8th 2021)/Material/Code/Demo 1 - Scratch.py
@@ -0,0 +1,21 @@
+from PIL import Image
+
+img = Image.open("messi5.jpg")
+print("Old dimens :", img.size)
+
+factor = 2
+
+W = img.width
+H = img.height
+
+newW = int(W*factor)  # width
+newH = int(H*factor)  # height
+
+newImage = Image.new("RGB", (newW, newH))
+for col in range(newW):
+    for row in range(newH):
+        p = img.getpixel((col/factor, row/factor))
+        newImage.putpixel((col, row), p)
+
+newImage.show()
+print("New dimens :", newImage.size)
